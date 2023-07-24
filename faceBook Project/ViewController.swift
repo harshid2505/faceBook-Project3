a801d796e774ed11aad587edb8208f07b024ba0d
Harshid Vaddoriya <harshidpatel388@gmail.com>
2023-07-24 14:01:29 +0530

    24

--- faceBook Project/ViewController.swift
+++ faceBook Project/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseFirestore
+import FirebaseCore
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var te: UITextField!
+    @IBOutlet weak var t2: UITextField!
+    @IBOutlet weak var t3: UITextField!
+    
+    var refa: Firestore!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        refa = Firestore.firestore()
     }
 
-
+    @IBAction func buttonAction(_ sender: Any) {
+       add()
+    }
+    
+    func add(){
+        refa.collection("Student").addDocument(data: ["ID":te.text!,"Name":t2.text!,"Gr":t3.text!])
+    }
+    
 }
 
