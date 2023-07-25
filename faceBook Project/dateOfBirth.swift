ceb6de10ad0f7a269316663367f78da2312fcf95
jenis navadiya <jenisnavadiya04@gmail.com>
2023-07-25 14:56:53 +0530

    txt

--- faceBook Project/dateOfBirth.swift
+++ faceBook Project/dateOfBirth.swift
@@ -1,10 +1,3 @@
-//
-//  dateOfBirth.swift
-//  Pods
-//
-//  Created by Jenish Navadiya on 25/07/23.
-//
-
 import UIKit
 
 class dateOfBirth: UIViewController {
@@ -12,18 +5,6 @@ class dateOfBirth: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
